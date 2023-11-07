32b4d3fc6bf8557fbdce94bf747d79f2a50721a7
kendall chapman <jkc4310@gmail.com>
2023-11-07 11:28:47 -0500

    first push

---
+++ VA Native Plants/VA Native Plants/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VA_Native_Plants.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
